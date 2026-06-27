068737e42146f8909bc41d688215e8420f8d2b5d
Doni Saputra <donisptra315@gmail.com>
2026-06-27 22:56:21 +0700

    Create KategoriModel for kategori table management

---
+++ backend/app/Models/KategoriModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppModels;
+
+use CodeIgniterModel;
+
+class KategoriModel extends Model
+{
+    protected $table = 'kategori';
+    protected $primaryKey = 'id_kategori';
+    protected $useAutoIncrement = true;
+    protected $allowedFields = ['nama_kategori', 'slug_kategori'];
+}
