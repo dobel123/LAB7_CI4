64370d98fb07b887c620629735593cf2caee0332
Doni Saputra <donisptra315@gmail.com>
2026-06-27 22:56:36 +0700

    Add UserModel for user data management

---
+++ backend/app/Models/UserModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppModels;
+
+use CodeIgniterModel;
+
+class UserModel extends Model
+{
+    protected $table = 'user';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $allowedFields = ['username', 'useremail', 'userpassword', 'token'];
+}
